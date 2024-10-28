6084f3047ef8f4936846d4d7961098409480d7c7
sanmijingbrahma <bsanmijing@gmail.com>
2024-10-28 18:11:21 +0530

    Set FPS and draw a circle on the center of window

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ int main()
     const int screen_width = 1280;
     const int screen_height = 800; 
     InitWindow(screen_width, screen_height, "Pong Game");
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        
 
-        
+            DrawCircle(screen_width/2,screen_height/2,40,WHITE);        
+
         EndDrawing();
     }
     CloseWindow();
